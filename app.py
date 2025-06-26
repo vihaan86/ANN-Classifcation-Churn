5e49c909389c96ddd0355202ca62134258ea1d70
vihaan86 <51486593+vihaan86@users.noreply.github.com>
2025-06-26 16:29:47 +0530

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import numpy as np
-import tensorflow as tf
+import tensorflow 
 from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
 import pandas as pd
 import pickle
