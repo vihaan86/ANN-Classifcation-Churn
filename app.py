a4095efef70c7b1164b02fa66eba27e2d4f1252c
vihaan86 <51486593+vihaan86@users.noreply.github.com>
2025-07-01 23:31:47 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import streamlit as st
 import numpy as np
-import tensorflow as tf
+from keras.models import load_model
 from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
 import pandas as pd
 import pickle
 
 
 # Load the trained model
-model = tf.keras.models.load_model('model.h5')
+model = load_model('model.h5')
 
 # Load the encoders and scaler
 with open('le.pkl', 'rb') as file:
